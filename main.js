52d2d3a021fdb68cdf3bd4dce6522adb6387a522
Mirsaid <akbaraliyev522@mail.ru>
2024-08-06 17:58:40 +0500

    main

---
+++ main.js
@@ -0,0 +1,136 @@
+// Home Task-1
+
+// let son = prompt("Son kiriting");
+// let son2 = prompt("Ikkinchi sonni kiriting");
+
+// for(let i = 1; i <= son2; i++){
+//     console.log(i);
+//     console.log(son);
+// }
+
+// ----------------------------------------------------------
+
+// Home Task-2
+
+// let a = 10;
+// let b = 20;
+
+// for(let i = a; a <= b; i++){
+//     console.log(i);
+// }
+
+// ----------------------------------------------------------
+
+
+// Home Task-3
+
+// let a = prompt("Son kiriting")
+// let b = prompt("Ikkinchi sonni kiriting")
+
+// for(let i = b -1 ; i > a; i--){
+//     console.log(i);
+// }
+
+// Home Task-4
+// ----------------------------------------------------------
+
+// let kg = 10;
+// let sum = 1500;
+
+// for(let i = 1; i <= kg; i++){
+//     console.log( i * sum);
+// }
+
+// ----------------------------------------------------------
+
+// Home Task-5
+
+// let a = 1;
+// let sum = 2000;
+// for (let i = 0; i <= a; i += 0.1){
+//     console.log(`${i.toFixed(1)}kg = ${i.toFixed(9) * sum} sum`);
+    
+// }
+
+// ----------------------------------------------------------
+
+//  Home Task-6
+
+// let a = prompt("Son kiriting")
+
+
+// if(a % 2 == 0 && a % 2 == 1){
+//     console.log("Siz kiritgan son Juft");
+// }else{
+//     console.log("Siz kiritgan son Toq");
+// }
+
+// ----------------------------------------------------------
+
+// Home Task-7
+
+// let a = prompt("Birinchi sonni kiriting")
+// let b = prompt("Ikkinchi sonni kiriting")
+
+// if ( a % 2 == 0 && b % 2 == 0 || a % 2 == 1 && b % 2 == 1){
+//     console.log("True");
+// }else{
+//     console.log("False");
+// }
+
+// ----------------------------------------------------------
+// Home Task-8
+
+// let a = prompt("Birinchi sonni kiriting")
+// let b = prompt("Ikkinchi sonni kiriting")
+// let c = prompt("Uchinchi sonni kiriting")
+
+// if (a > 0 && b > 0 && c > 0){
+//     console.log("Siz kiritgan Sonlar musbat");
+// }else{
+//     console.log("Siz kiritgan sonlar manfiy");
+// }
+
+// ----------------------------------------------------------
+
+// Home Task-9
+
+// let a = prompt("Birinchi sonni kiriting")
+// let b = prompt("Ikkinchi sonni kiriting")
+// let c = prompt("Uchinchi sonni kiriting")
+
+// if (a > 0 || b > 0 || c > 0){
+//     console.log("True");
+// }else{
+//     console.log("False");
+// }
+
+// ----------------------------------------------------------
+
+
+// Home Task- 10
+
+// let a = prompt("Birinchi sonni kiriting")
+// let b = prompt("Ikkinchi sonni kiriting")
+// let c = prompt("Uchinchi sonni kiriting")
+
+// if (a > 0 && b < 0 && c < 0 || b > 0 && c < 0 && a < 0 || c > 0 && b < 0 && a < 0){
+//     console.log("True");
+// }else{
+//     console.log("False");
+// }
+
+// ----------------------------------------------------------------------------------
+
+// Home Task-11
+
+// let a = prompt("Birinchi sonni kiriting")
+// let b = prompt("Ikkinchi sonni kiriting")
+// let c = prompt("Uchinchi sonni kiriting")
+
+// if (a < 0 && b > 0 && c > 0 || b < 0 && c > 0 && a > 0 || c < 0 && b > 0 && a > 0){
+//     console.log("True");
+// }else{
+//     console.log("False");
+// }
+
